297a0fa2926e05209f55fdfab651750905692866
mgrange <mackay.grange@gmail.com>
2024-04-23 18:25:47 -0600

    fixed lualine lsp error icons and background. No longer doubles erros as well.

--- lua/mgrange/config/lualine.lua
+++ lua/mgrange/config/lualine.lua
@@ -1,60 +1,61 @@
-require('lualine').setup 
+require('lualine').setup
 {
-    options = 
+    options =
     {
         icons_enabled = true,
         theme = 'auto',
         component_separators = { left = '', right = ''},
         section_separators = { left = '', right = ''},
-        disabled_filetypes = 
+        disabled_filetypes =
         {
             statusline = {},
             winbar = {},
         },
         ignore_focus = {},
         always_divide_middle = true,
-        globalstatus = true,  
-        refresh = 
+        globalstatus = true,
+        refresh =
         {
             statusline = 1000,
             tabline = 1000,
             winbar = 1000,
         }
     },
-    sections = 
+    sections =
     {
-        lualine_a = {'mode'},
-        lualine_b = 
-        {'branch', 'diff', 
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff'},
+        lualine_c =
         {
-            'diagnostics', 
-            sources = {'nvim_lsp', 'nvim_diagnostic'}, 
-            sections = {'error', 'warn', 'info', 'hint'},
-            diagnostics_color = 
             {
-                error = 'DiagnosticError',
-                warn = 'DiagnosticWarn',
-                info = 'DiagnosticInfo',
-                hint = 'DiagnosticHing',
+                'diagnostics',
+                sources = {'nvim_lsp'},
+                sections = {'error', 'warn', 'info', 'hint'},
+                diagnostics_color =
+                {
+                    error = 'DiagnosticError',
+                    warn = 'DiagnosticWarn',
+                    info = 'DiagnosticInfo',
+                    hint = 'DiagnosticHing',
+                },
+                symbols =
+                {
+                    error = '',
+                    warn = '',
+                    info = '',
+                    hint = '󰌵',
+                },
+                colored = false,
+                update_in_insert = true,
+                always_visible = false,
             },
-            symbols = 
-            {
-                error = 'E',
-                warn = 'W',
-                info = 'I',
-                hint = 'H',
-            },
-            colored = true,
-            update_in_insert = false,
-            always_visible = false,
+            {'filename', path = 0, file_status = true, shorting_target = 40},
         },
-        },
-        lualine_c = {{'filename', path = 0, file_status = true, shorting_target = 40},},
         lualine_x = { {'encoding', fmt = string.upper }, 'fileformat', 'filetype'},
         lualine_y = {'progress'},
         lualine_z = {'location'}
     },
-    inactive_sections = 
+    inactive_sections =
     {
         lualine_a = {},
         lualine_b = {},
@@ -66,5 +67,5 @@ require('lualine').setup
     tabline = {},
     winbar = {},
     inactive_winbar = {},
-    extensions = {'lazy', 'fugitive', 'fzf', 'mason', 'neo-tree'},
+    extensions = {'lazy', 'fugitive', 'mason', 'neo-tree'},
 }
